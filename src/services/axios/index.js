a756a2f8fabf3b0525fdfe55faea38e1ed769ca0
Yunus Emre BAKAÇ <yunusemrefor@gmail.com>
2020-11-02 23:50:23 +0300

    fix: CORS problem

--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "../../store";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8080/api",
+  baseURL: "/api",
   timeout: 5000
 });
 const token = localStorage.getItem("token");
